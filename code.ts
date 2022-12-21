d592548b3924712b84006da9f3459e852ed59c26
Rabire <rabireh@outlook.fr>
2022-12-21 15:44:58 +0100

    change component text content

--- code.ts
+++ code.ts
@@ -2,7 +2,9 @@ figma.showUI(__html__);
 
 figma.ui.resize(500, 500);
 
-figma.ui.onmessage = (pluginMessage) => {
+figma.ui.onmessage = async (pluginMessage) => {
+  await figma.loadFontAsync({ family: "Rubik", style: "Regular" });
+
   const postComponentSet = figma.root.findOne(
     (node) => node.type == "COMPONENT_SET" && node.name == "post"
   ) as ComponentSetNode;
@@ -27,7 +29,21 @@ figma.ui.onmessage = (pluginMessage) => {
         }`
   ) as ComponentNode;
 
-  componentToRender.createInstance();
+  const newPost = componentToRender.createInstance();
+
+  const templateName = newPost.findOne(
+    (node) => node.name === "displayName" && node.type === "TEXT"
+  ) as TextNode;
+  const templateUsername = newPost.findOne(
+    (node) => node.name == "@username" && node.type == "TEXT"
+  ) as TextNode;
+  const templateDescription = newPost.findOne(
+    (node) => node.name == "description" && node.type == "TEXT"
+  ) as TextNode;
+
+  templateName.characters = pluginMessage.name;
+  templateUsername.characters = pluginMessage.username;
+  templateDescription.characters = pluginMessage.description;
 
   figma.closePlugin();
 };
