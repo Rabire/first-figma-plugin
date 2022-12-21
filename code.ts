9ca3e5cf9217fab23a2e53c9a554ea4605aff9d9
Rabire <rabireh@outlook.fr>
2022-12-21 15:28:33 +0100

    conditional creating instance

--- code.ts
+++ code.ts
@@ -3,7 +3,31 @@ figma.showUI(__html__);
 figma.ui.resize(500, 500);
 
 figma.ui.onmessage = (pluginMessage) => {
-  console.log(pluginMessage);
+  const postComponentSet = figma.root.findOne(
+    (node) => node.type == "COMPONENT_SET" && node.name == "post"
+  ) as ComponentSetNode;
+
+  const getImageVariant = () => {
+    switch (pluginMessage.imageVariant) {
+      case "2":
+        return "single";
+      case "3":
+        return "carousel";
+      default:
+        return "none";
+    }
+  };
+
+  const componentToRender = postComponentSet.findOne(
+    (node) =>
+      node.type == "COMPONENT" &&
+      node.name ==
+        `Image=${getImageVariant()}, Dark mode=${
+          pluginMessage.darkModeState ? "true" : "false"
+        }`
+  ) as ComponentNode;
+
+  componentToRender.createInstance();
 
   figma.closePlugin();
 };
